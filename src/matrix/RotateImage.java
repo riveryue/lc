9015b132b818da48743c80aeba5f342ec91a68ef
yao <470238714@qq.com>
2024-04-30 19:25:18 +0800

    rotate

---
+++ src/matrix/RotateImage.java
@@ -0,0 +1,19 @@
+package matrix;
+
+public class RotateImage {
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int[][] matrix_new = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix_new[j][n - i - 1] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                matrix[i][j] = matrix_new[i][j];
+            }
+        }
+    }
+}
