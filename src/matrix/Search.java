2eac5ebd4a97d31b324b7f2bf26466194781dd00
yao <470238714@qq.com>
2024-05-01 22:47:19 +0800

    search

---
+++ src/matrix/Search.java
@@ -0,0 +1,14 @@
+package matrix;
+
+public class Search {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                if (element == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
