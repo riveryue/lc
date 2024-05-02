cfb6ac0c21195f3c5c70e10098ade49fddd35729
yao <470238714@qq.com>
2024-05-02 20:56:31 +0800

    internode

---
+++ src/listnode/InterNode.java
@@ -0,0 +1,23 @@
+package listnode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class InterNode {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<ListNode>();
+        ListNode temp = headA;
+        while (temp != null) {
+            visited.add(temp);
+            temp = temp.next;
+        }
+        temp = headB;
+        while (temp != null) {
+            if (visited.contains(temp)) {
+                return temp;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+}
