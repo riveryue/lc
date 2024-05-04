abbbb2492e3eefb606250a5c3a927c1d8ab82d8b
yao <470238714@qq.com>
2024-05-04 19:18:22 +0800

    palidrome

---
+++ src/listnode/Palidrome.java
@@ -0,0 +1,29 @@
+package listnode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Palidrome {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> vals = new ArrayList<Integer>();
+
+        // 将链表的值复制到数组中
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        // 使用双指针判断是否回文
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            if (!vals.get(front).equals(vals.get(back))) {
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
+}
