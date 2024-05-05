fc073f24fbe328059f3b80cd2cd697bdb378a8ef
yao <470238714@qq.com>
2024-05-05 19:29:41 +0800

    cycle list node

---
+++ src/listnode/CycleListNode.java
@@ -0,0 +1,17 @@
+package listnode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CycleListNode {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> seen = new HashSet<ListNode>();
+        while (head != null) {
+            if (!seen.add(head)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+}
