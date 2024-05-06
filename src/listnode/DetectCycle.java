e8dfb418db82ec89c1cf80f68bff0403a01aa181
yao <470238714@qq.com>
2024-05-06 22:47:28 +0800

    detect cycle

---
+++ src/listnode/DetectCycle.java
@@ -0,0 +1,20 @@
+package listnode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DetectCycle {
+    public ListNode detectCycle(ListNode head) {
+        ListNode pos = head;
+        Set<ListNode> visited = new HashSet<ListNode>();
+        while (pos != null) {
+            if (visited.contains(pos)) {
+                return pos;
+            } else {
+                visited.add(pos);
+            }
+            pos = pos.next;
+        }
+        return null;
+    }
+}
