c04c92fc8da65e2d477a08d37d7f8db2cb5c2408
yao <470238714@qq.com>
2024-05-27 20:35:25 +0800

    flatten

---
+++ src/tree/Faltten.java
@@ -0,0 +1,25 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Faltten {
+    public void flatten(TreeNode root) {
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        preorderTraversal(root, list);
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            TreeNode prev = list.get(i - 1), curr = list.get(i);
+            prev.left = null;
+            prev.right = curr;
+        }
+    }
+
+    public void preorderTraversal(TreeNode root, List<TreeNode> list) {
+        if (root != null) {
+            list.add(root);
+            preorderTraversal(root.left, list);
+            preorderTraversal(root.right, list);
+        }
+    }
+}
